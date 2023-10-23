4562f141e362a48acb3aa8652bfa52a91debb8b5
lvlivingston <lvlivingston@gmail.com>
2023-10-23 18:18:02 -0500

    MVP completed homework

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const ROOT_URL = 'https://catfact.ninja/fact';
+const ROOT_URL = 'https://catfact.ninja';
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const catData = await fetch(`${ROOT_URL}/cat/${fact}`, options)
-    .then(res => res.json());
-  catData.repos = await fetch(catData.repos_url, options)
-    .then(res => res.json());
-  res.render('index', { catData });
+  // console.log(catData);
+  // console.log(catJson);
+  
+  res.render('index');
 });
+
+router.get('/cats/fact', async function(req, res) {
+  const catData = await fetch(`${ROOT_URL}/fact`)
+  const catJson = await catData.json();
+  res.render('cat/fact', { fact: catJson.fact });
+})
 
 module.exports = router;
